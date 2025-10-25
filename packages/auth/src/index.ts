b8f1f4164a42d0f8323ef1aab278e5c27213eb25
Omar Sabry <omarahmedsabry05@gmail.com>
2025-10-25 14:19:15 +0300

    Remove broken user-service submodule

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,11 @@
+// Guards
+export * from './guard/role.guard';
+
+// Decorators
+export * from './decorators/auth.decorator';
+export * from './decorators/currentUser.decorator';
+
+// Constants
+export * from './constants/enum.constant';
+export * from './constants/rolePermissionsMap.constant';
+export * from './constants/messages.constant';
